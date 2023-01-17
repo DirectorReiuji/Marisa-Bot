93fa21ff7f808e4851673c6195ac2ff349d87673
Marisa Gaming <gamershadow874@gmail.com>
2023-01-16 20:48:57 -0600

    Added options to /touhou
    Added options to /touhou for more searches, and adds a disclaimer just incase the image fails to load.

--- src/commands/fun/touhou.js
+++ src/commands/fun/touhou.js
@@ -5,11 +5,23 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('touhou')
-        .setDescription('Grabs posts from r/Touhou'),
+        .setDescription('Grabs posts from Touhou related subreddits.')
+    	.addStringOption((option) =>
+			option
+				.setName('subreddit')
+                .setDescription('The subreddit to grab posts from.')
+                .setRequired(true)
+                .addChoices(
+            		{ name: 'r/Touhou', value: 'touhou' },
+            		{ name: 'r/2hujerk', value: '2hujerk' },
+            		{ name: 'r/fumotouhou', value: 'fumotouhou' }
+                )
+    	),
     async execute(interaction, client) {
         const embed = new EmbedBuilder();
+        const option = interaction.options.getString('subreddit');
         
-        let data = await fetch("http://www.reddit.com/r/touhou/random/.json").then(async res => {
+        let data = await fetch(`http://www.reddit.com/r/${option}/random/.json`).then(async res => {
         	let post = await res.json();
             
             let title = post[0].data.children[0].data.title;
@@ -17,7 +29,7 @@ module.exports = {
             let upvotes = post[0].data.children[0].data.ups;
             
             return interaction.reply({ embeds: [
-                embed.setColor("Gold").setFooter({ text: upvotes + " Upvotes", iconURL: interaction.guild.iconURL() }).setTimestamp().setTitle(title).setURL(url).setImage(url)
+                embed.setColor("Gold").setFooter({ text: upvotes + " Upvotes", iconURL: interaction.guild.iconURL() }).setTimestamp().setTitle(title).setURL(url).setImage(url).setDescription("*If the image doesn't load/work, please press the title...*")
             ]})
         });
 	}

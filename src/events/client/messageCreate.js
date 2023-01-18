c2e13091ff34f89bb7178b65504df9a88fdcc85d
Marisa Gaming <gamershadow874@gmail.com>
2023-01-17 22:57:30 -0600

    FUCK IT, WE BALLIN
    FUCK IT, WE BALLIN

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -8,7 +8,7 @@ module.exports = {
         triggerWords.forEach((word) => {
     		if (message.content.includes(word)) {
                 if (word === 'Mhelp') {
-                    message.reply('Currently the message reactions are:\n\`Mbnuuy\`, \`Mmarisad\`, \`Mredux\`, \`Moguu\`, \`Mgoogle\`, \`Mfishy\`, \`Mgoblin\`, \`Mlooking\`, and \`Mviolence\`.');
+                    message.reply('Currently the message reactions are:\n`'.concat(triggerWords).replaceAll(',', '`, `') + '`.');
 				} else if (word === 'Mbnuuy') {
                     message.reply('https://cdn.discordapp.com/attachments/598596152719442027/1050483386101272699/trim.AD0CF02B-389C-45CF-B886-7E199AD6C820.mov');
                 } else if (word === 'Mmarisad') {
@@ -22,7 +22,7 @@ module.exports = {
                 } else if (word === 'Mfishy') {
                     message.reply("https://cdn.discordapp.com/attachments/414559232348913674/1062949336692039690/video01.mov");
                 } else if (word === 'Mgoblin') {
-                    message.reply("https://cdn.discordapp.com/attachments/715273567809568849/1064747483290402937/WE_ARE_JAPANESE_GOBLIN_MMD.mp4");
+                    message.reply("https://cdn.discordapp.com/attachments/715273567809568849/1065132418606047242/WE_ARE_JAPANESE_GOBLIN.mp4");
                 } else if (word === 'Mlooking') {
                     message.reply("https://cdn.discordapp.com/attachments/598596152719442027/1064752769690521610/IMG_8524.jpg");
                 } else if (word === 'Mviolence') {
